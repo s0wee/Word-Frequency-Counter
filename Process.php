00fb303e41f20356d378dfff303a5f2e944569fd
s0wee <khalilangeloacosta@gmail.com>
2023-10-01 13:41:38 +0800

    practice #2

--- Process.php
+++ Process.php
@@ -10,34 +10,34 @@
         $words = str_word_count($input_text, 1);
         $stopWords = ["the", "and", "in"];
         $wordCounts = array_count_values(array_diff($words, $stopWords));
-        if ($pattern === "asc") 
-        {
-            asort($wordCounts);
-        } 
-        else 
-        {
-            arsort($wordCounts);
-        }
+        	if ($pattern === "asc") 
+        	{
+           	 asort($wordCounts);
+        	} 
+        	else 
+        	{
+            	arsort($wordCounts);
+        	}
         
         $wordCounts = array_slice($wordCounts, 0, $limit);
         return $wordCounts;
     }
 
-    if (!empty($input_text)) 
+    	if (!empty($input_text)) 
     {
-        $i = 0;
-        $wordFrequencies = calculate_Word_Frequency($input_text, $pattern, $limit);
-        echo "<h2>Word Frequency:</h2>";
-        echo "<ul>";
+        	$i = 0;
+        	$wordFrequencies = calculate_Word_Frequency($input_text, $pattern, $limit);
+        	echo "<h2>Word Frequency:</h2>";
+        	echo "<ul>";
 
-        foreach ($wordFrequencies as $word => $frequency) {
-            echo "<li>$word: $frequency</li>";
+        	foreach ($wordFrequencies as $word => $frequency) {
+            	echo "<li>$word: $frequency</li>";
         }
 
-        echo "</ul>";
+        	echo "</ul>";
     } 
     
-    else {
-        echo "<p>Please enter some text.</p>";
+    	else {
+        	echo "<p>Please enter some text.</p>";
     }
 ?>
